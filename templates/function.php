c12c7197cb2679bbfaa9b7aaeff240511f61d726
adoboeater415 <pdayag1088@gmail.com>
2017-05-04 10:31:43 -0700

    Add files via upload

---
+++ templates/function.php
@@ -0,0 +1,17 @@
+<?php 
+
+/*
+Theme Name: Demo Theme for Web 170 Spring 2017
+Theme URI: https://github.com/adoboeater415/web170/tree/master/templates
+Author: Pierre Dayag
+Author URI: http;//pierredayag.com
+Description: This is my Demo Theme for my Web 170 class in the Spring Quarter 2017
+Version: 42.0
+*/
+
+// Register Menu Locations
+
+register_nav_menus(array('main-menu' => __('Main Menu'), 'footer-menu' => __('Footer Menu'),));
+
+//
+?>
